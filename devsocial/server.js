b2a19d63d80b58ed8262e3c8b0a9ae46cc7a2518
Eric Abenojar <abenoeric@gmail.com>
2018-06-28 02:32:00 -0700

    First commit express server

---
+++ devsocial/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const users = require("./routes/api/user");
+const posts = require("./routes/api/posts");
+const profile = require("./routes/api/profile");
+
+const app = express();
+
+// DB Config
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log("MONGODB CONNECTED"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => res.send("Hello World"));
+
+// Use Routes
+
+app.use("/api/users", users);
+app.use("/api/posts", posts);
+app.use("/api/profile", profile);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
